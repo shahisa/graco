fceee357a49ae0ad92cb5fbdf66327c2f0e83dca
Isaiah Weaver <iswea1@morgan.edu>
2018-10-04 23:41:34 -0400

    added image

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import courtHouseImg from './courtHouseImg.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Graço: Make Your Friends Place Your Personal Warehouse</h1>
         </header>
         <p className="App-intro">
           <h2> How It Works:</h2>
-            If you are moving out for the semester, going on internship, studying abroad or whatever life throws at you can request a person
-            store your stuff.  
-
-
-
+            If you are moving out for the semester, have a long layover, or just tired of carrying your bags, now you
+            have a place to drop store them.
         </p>
         <p>
           Avalible Storage Areas: Baltimore, MD
         </p>
+        <div className="App-image">
+          <img src={courtHouseImg} alt="Court House Aparments "/>
+        </div>
         <p>
           1. 6605 Collinsdale Rd 2. 22 Acorn Circle
         </p>
+
       </div>
+
     );
   }
 }
