2a04b56cd07bffbad29cc2ccdf308441dbce4f70
Isaiah Weaver <iswea1@morgan.edu>
2018-10-12 00:01:39 -0400

    added nav.js component

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import courtHouseImg from './courtHouseImg.jpg'
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Home from "./components/Home";
 import About from "./components/About";
@@ -11,11 +11,11 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div>
+        <Switch>
           <Route path="/" component ={Home} exact />
           <Route path="/about" component ={About}  />
 
-        </div>
+        </Switch>
       </BrowserRouter>
       //   <div className="App-image">
       //     <img src={courtHouseImg} alt="Court House Aparments "/>
